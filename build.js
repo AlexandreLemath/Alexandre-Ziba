b2c369b96ea2aeeeedbff59c7559ffc29169998f
Alexandre <alexandreziba245@gmail.com>
2025-04-22 19:24:20 +0200

    Update index.html and remove build.js from repo

--- build.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const fs = require('fs');
-const id = process.env.FORMSPREE_ID;
-if (!id) {
-  console.error('FORMSPREE_ID manquant dans .env');
-  process.exit(1);
-}
-fs.writeFileSync('config.js', `window.FORMSPREE_ID = "${id}";`);
